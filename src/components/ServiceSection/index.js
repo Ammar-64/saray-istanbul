c8200deea0974e216fc4bc098651ddd0955d3cea
your_name <your_email@example.com>
2021-11-28 23:48:01 +0300

    services section home page

--- src/components/ServiceSection/index.js
+++ src/components/ServiceSection/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
 import serviceImg1 from "../../img/service-bg-1.png";
 import serviceImg2 from "../../img/service-bg-2.jpg";
 import serviceImg3 from "../../img/service-bg-3.jpg";
@@ -11,11 +11,34 @@ import serviceIcon3 from "../../img/service-icon-3.png";
 import "./style.css";
 
 const ServiceSection = () => {
+  const [t] = useTranslation();
   return (
     <section className="service-area">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-lg-4 col-md-6">
+          {t("cards", { returnObjects: true }).map((card) => (
+            <div className="col-lg-4 col-md-6">
+              <div
+                className="service-box"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <div className="service-icon">
+                  {/* <img src={serviceIcon1} alt="img" /> */}
+                  <i className={card.img} alt="img"></i>
+                </div>
+                <div className="service-text">
+                  <h3>{card.title}</h3>
+                  <p>{card.description}</p>
+                  {/* <Link to="/service-single" className="cta-btn btn-border">
+                    Read More
+                  </Link> */}
+                </div>
+                {/* <img src={serviceImg1} alt="img" /> */}
+              </div>
+            </div>
+          ))}
+          {/* <div className="col-lg-4 col-md-6">
             <div
               className="service-box"
               data-aos="fade-up"
@@ -80,7 +103,7 @@ const ServiceSection = () => {
               </div>
               <img src={serviceImg3} alt="img" />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
