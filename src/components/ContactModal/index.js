cd541fdcc18f6f3b7dd43028d51e910eadbd1955
Ammar-64 <ammar.n.sammour@gmail.com>
2022-11-24 03:06:13 +0300

    delete default country code

--- src/components/ContactModal/index.js
+++ src/components/ContactModal/index.js
@@ -100,7 +100,6 @@ const ContactForm = () => {
                 <div className="form-field">
                   <PhoneInput
                     value={state.phoneNumber}
-                    defaultCountry="TR"
                     labels={ar}
                     onChange={phoneHandler}
                     placeholder={t("contactUsPage.contactForm.phone")}
