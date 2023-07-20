a7fc3c0a908e02948420e345783fd0c09b58bc77
Ammar-64 <ammar.n.sammour@gmail.com>
2023-07-20 16:09:00 +0100

    change colors, texts and images

--- src/components/PrivacyPolicy/index.js
+++ src/components/PrivacyPolicy/index.js
@@ -1,11 +1,11 @@
 const PrivacyPolicy = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Privacy Policy for Saray Istanbul</h1>
+      <h1>Privacy Policy for Akar Turk</h1>
       <p>
-        At Saray Istanbul, one of our main priorities is the privacy of our
-        visitors. This Privacy Policy document contains types of information
-        that is collected and recorded by Saray Istanbul and how we use it.
+        At Akar Turk, one of our main priorities is the privacy of our visitors.
+        This Privacy Policy document contains types of information that is
+        collected and recorded by Akar Turk and how we use it.
       </p>
       <p>
         If you have additional questions or require more information about our
@@ -13,14 +13,14 @@ const PrivacyPolicy = () => {
       </p>
       <h2>Log Files</h2>
       <p>
-        Saray Istanbul follows a standard procedure of using log files. These
-        files log visitors when they use app. The information collected by log
-        files include internet protocol (IP) addresses, browser type, Internet
-        Service Provider (ISP), date and time stamp, referring/exit pages, and
-        possibly the number of clicks. These are not linked to any information
-        that is personally identifiable. The purpose of the information is for
-        analyzing trends, administering the app, tracking users' movement on the
-        app, and gathering demographic information.
+        Akar Turk follows a standard procedure of using log files. These files
+        log visitors when they use app. The information collected by log files
+        include internet protocol (IP) addresses, browser type, Internet Service
+        Provider (ISP), date and time stamp, referring/exit pages, and possibly
+        the number of clicks. These are not linked to any information that is
+        personally identifiable. The purpose of the information is for analyzing
+        trends, administering the app, tracking users' movement on the app, and
+        gathering demographic information.
       </p>
       <h2>Our Advertising Partners</h2>
       <p>
@@ -42,23 +42,23 @@ const PrivacyPolicy = () => {
       <h2>Privacy Policies</h2>
       <p>
         You may consult this list to find the Privacy Policy for each of the
-        advertising partners of Saray Istanbul.
+        advertising partners of Akar Turk.
       </p>
       <p>
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Beacons that are used in their respective advertisements
-        and links that appear on Saray Istanbul. They automatically receive your
-        IP address when this occurs. These technologies are used to measure the
+        and links that appear on Akar Turk. They automatically receive your IP
+        address when this occurs. These technologies are used to measure the
         effectiveness of their advertising campaigns and/or to personalize the
         advertising content that you see on this app or other apps or websites.
       </p>
       <p>
-        Note that Saray Istanbul has no access to or control over these cookies
-        that are used by third-party advertisers.
+        Note that Akar Turk has no access to or control over these cookies that
+        are used by third-party advertisers.
       </p>
       <h2>Third Party Privacy Policies</h2>
       <p>
-        Saray Istanbul's Privacy Policy does not apply to other advertisers or
+        Akar Turk's Privacy Policy does not apply to other advertisers or
         websites. Thus, we are advising you to consult the respective Privacy
         Policies of these third-party ad servers for more detailed information.
         It may include their practices and instructions about how to opt-out of
@@ -71,7 +71,7 @@ const PrivacyPolicy = () => {
         participate in, and/or monitor and guide their online activity.
       </p>
       <p>
-        Saray Istanbul does not knowingly collect any Personal Identifiable
+        Akar Turk does not knowingly collect any Personal Identifiable
         Information from children under the age of 13. If you think that your
         child provided this kind of information on our App, we strongly
         encourage you to contact us immediately and we will do our best efforts
@@ -81,7 +81,7 @@ const PrivacyPolicy = () => {
       <p>
         This Privacy Policy applies only to our online activities and is valid
         for visitors to our App with regards to the information that they shared
-        and/or collect in Saray Istanbul. This policy is not applicable to any
+        and/or collect in Akar Turk. This policy is not applicable to any
         information collected offline or via channels other than this app. Our
         Privacy Policy was created with the help of the{" "}
         <a href="https://www.app-privacy-policy.com/app-privacy-policy-generator/">
